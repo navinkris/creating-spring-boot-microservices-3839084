39b6f36b6a8a9ba620b3eaf6d7eed34418c2cce5
Navin Krishnamurthi <navinkrishnamurthi9994@gmail.com>
2025-02-10 14:49:38 +0000

    Changed the method to findByName from findByID

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/business/TourService.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/business/TourService.java
@@ -23,8 +23,8 @@ public class TourService {
       String description, String blurb, Integer price, String duration,
       String bullets, String keywords, Difficulty difficulty, Region region) {
 
-    TourPackage tourPackage = tourPackageRepository.findById(tourPackageName)
-      .orElseThrow(() -> new RuntimeException("Tour Package not found for id:" + tourPackageName));
+    TourPackage tourPackage = tourPackageRepository.findByName(tourPackageName)
+        .orElseThrow(() -> new RuntimeException("Tour Package not found for id:" + tourPackageName));
     return tourRepository.save(new Tour(title, description, blurb,
         price, duration, bullets, keywords, tourPackage, difficulty, region));
   }
